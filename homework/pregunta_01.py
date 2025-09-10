e84c3ac55626e5394e8e519ab45ce167f7ad3128
danielgonzalez2709 <dgonzalezhe@unal.edu.co>
2025-09-10 11:01:30 -0500

    Hola mundo

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,3 +1,4 @@
+
 """
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
@@ -8,13 +9,8 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 def pregunta_01():
     """
     Retorne el string "Hola mundo cruel!".
-
-    Rta/
-    Hola mundo cruel!
-
     """
-
-    return
+    return "Hola mundo cruel!"
 
 
 if __name__ == "__main__":
--- homework/pregunta_02.py
+++ homework/pregunta_02.py
@@ -1,3 +1,4 @@
+
 """
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
@@ -8,13 +9,8 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 def pregunta_02():
     """
     Retorne el string "Hello cruel world!".
-
-    Rta/
-    Hello cruel world!
-
     """
-
-    return
+    return "Hello cruel world!"
 
 
 if __name__ == "__main__":
